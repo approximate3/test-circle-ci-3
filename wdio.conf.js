3becb339767a6bea631ce0ea51dc371c943b1379
approximate3 <115112451+approximate3@users.noreply.github.com>
2022-10-05 14:14:08 -0700

    testing base URL change

--- wdio.conf.js
+++ wdio.conf.js
@@ -94,8 +94,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
-    port: 4444,
+    baseUrl: 'http://127.0.0.1:4000',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
